85c2deeb0b88ca196f10a280cd57c5eee058d1d0
Lívia Caires Lançoni <livialanconi2009@gmail.com>
2025-12-02 08:37:43 -0300

    .

---
+++ MVC/CadAlunoTorloni/Controllers/FrutasController.cs
@@ -0,0 +1,69 @@
+using System.Runtime.CompilerServices;
+using CadAlunoTorloni.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadAlunoTorloni.Controllers
+{
+    public class FrutasController : Controller
+    {
+        private readonly ILogger<FrutasController> _logger;
+
+        public FrutasController(ILogger<FrutasController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        //criar uma lista de frutas
+        //citricas
+        //tropicais
+
+        private static List<Fruta> frutas = new List<Fruta>
+        {
+            new Fruta{ Id = 1, Nome = "Maça", Cor = "Vermelha", Categoria = "Tropical"},
+            new Fruta{ Id = 2, Nome = "Banana", Cor = "Amarela", Categoria = "Tropical"},
+            new Fruta{ Id = 3, Nome = "Uva", Cor = "Roxa", Categoria = "Tropical"},
+            new Fruta{ Id = 4, Nome = "Limão", Cor = "Verde", Categoria = "Cítrica"},
+            new Fruta{ Id = 5, Nome = "Abacaxi", Cor = "Amarela", Categoria = "Cítrica"},
+            
+
+        };
+
+        public IActionResult Index()
+        {
+            return View(frutas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Fruta fruta)
+        {
+            //cria o proximo ID
+            fruta.Id = frutas.Max(f => f.Id) + 1;
+            //salvar no array
+            frutas.Add(fruta);
+            //redirecionar o usuario para a Index
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult FrutasCitricas()
+        {
+            return View(frutas);
+        }
+        
+        public IActionResult FrutasTropicais()
+            {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ MVC/CadAlunoTorloni/Models/Fruta.cs
@@ -0,0 +1,15 @@
+
+namespace CadAlunoTorloni.Models
+{
+    public class Fruta
+    {
+        //id
+        //Nome
+        //Cor
+
+        public int Id;
+        public string Nome = "";
+        public string Cor;
+        public string Categoria;
+    }
+}
